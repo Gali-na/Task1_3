5a3b20e99a851b9be562adc65b5c78c3747b9a7e
Tsivka Galina <galinatsivka@gmail.come>
2020-07-09 11:17:34 +0300

    hw_1_3

--- src/test/java/BinaryStringTest.java
+++ src/test/java/BinaryStringTest.java
@@ -1,4 +1,16 @@
-package PACKAGE_NAME;
+import org.junit.Assert;
+import org.junit.Test;
 
 public class BinaryStringTest {
+    @Test
+    public void toBinaryString() {
+        BinaryString binaryString = new BinaryString();
+        for (int value = 0; value < 20; value++) {
+            String actualResult = binaryString.toBinaryString(value);
+            String expectedResult = Integer.toBinaryString(value);
+
+            Assert.assertEquals("Test failed with value: " + value,
+                    expectedResult, actualResult);
+        }
+    }
 }
